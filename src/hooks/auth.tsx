77370bf76a408e59215fad38812db4a8e8c21871
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-20 13:32:06 -0300

    Buscando providers da API

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -41,6 +41,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
+
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
       }
 
       setIsLoading(false);
@@ -58,6 +60,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,33 @@
-import React, { useCallback } from 'react';
-import { View, Button } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
 import {
-  Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar,
+  Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar, ProvidersList,
 } from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
+  useEffect(() => {
+    api.get('providers')
+      .then((response) => setProviders(response.data));
+  }, []);
+
   const navigateToProgile = useCallback(() => {
-    navigate('Profile');
+    // navigate('Profile');
+    signOut();
   }, [navigate]);
 
   return (
@@ -29,6 +43,16 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        renderItem={({ item }) => (
+          <UserName>
+            {item.name}
+          </UserName>
+        )}
+      />
     </Container>
   );
 };
