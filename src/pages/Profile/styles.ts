cbed4afa907e7221819136372f248d1b1b57eb37
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-24 14:25:32 -0300

    Finalização

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components/native';
 import { Platform } from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
-  justify-content: center;
   padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
-  position: relative;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.Text`
@@ -28,4 +31,18 @@ export const UserAvatar = styled.Image`
   height: 186px;
   border-radius: 98px;
   align-self: center;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  margin: 32px 0;
+  flex-direction: row;
+`;
+
+export const SignOutButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #ef3601;
+  font-size: 18px;
+  margin-left: 10px;
 `;
