b184f97243a1270f7478275424dab359910f8f58
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2020-12-24 12:32:17 -0300

    Criando tela de perfil

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -15,6 +15,7 @@ import { Container, TextInput, Icon } from './styles';
 interface InputProps extends TextInputProps {
   name: string;
   icon: string;
+  containerStyle?: {}
 }
 
 interface InputValueReference {
@@ -26,7 +27,9 @@ interface InputRef {
 }
 
 const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
-  { name, icon, ...rest },
+  {
+    name, icon, containerStyle = {}, ...rest
+  },
   ref,
 ) => {
   const [isFocused, setIsFocused] = useState(false);
@@ -71,7 +74,7 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused} isErrored={!!error}>
+    <Container style={containerStyle} isFocused={isFocused} isErrored={!!error}>
       <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
       <TextInput
         ref={inputElementRef}
